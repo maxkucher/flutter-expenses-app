8a28e746c31bdd707d2e054822990b6e99ec5813
Maksym Kucher <the.stewk@gmail.com>
2019-07-12 21:39:06 -0700

    - Finished deleting from card functionality.

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:tasks_app/models/cart_item.dart';
 
 class Cart with ChangeNotifier {
-  Map<String, CartItem> _items ={};
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items => {..._items};
 
@@ -10,12 +10,10 @@ class Cart with ChangeNotifier {
     return _items.length;
   }
 
-
   double get totalAmount {
     var total = 0.0;
-    _items.forEach((_,CartItem value) => {
-        total += value.price*value.quantity
-    });
+    _items.forEach(
+        (_, CartItem value) => {total += value.price * value.quantity});
     return total;
   }
 
@@ -38,6 +36,11 @@ class Cart with ChangeNotifier {
               price: price));
     }
 
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
     notifyListeners();
   }
 }
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tasks_app/models/cart_item.dart';
 import 'package:tasks_app/providers/cart.dart';
+import 'package:tasks_app/widgets/cart_list_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
+
   @override
   Widget build(BuildContext context) {
     final cart = Provider.of<Cart>(context);
@@ -17,24 +20,52 @@ class CartScreen extends StatelessWidget {
           Card(
             margin: EdgeInsets.all(15),
             child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Row(
-                children: <Widget>[
-                  Text('Total',
-                  style: TextStyle(
-                    fontSize: 20
-                  ),
-                  ),
-                  SizedBox(width: 10),
-                  Chip(label: Text('\$${cart.totalAmount}'),
-                  backgroundColor: Theme.of(context).primaryColor,)
-                ],
-              )
-            ),
-          )
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text(
+                      'Total',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Chip(
+                      label: Text(
+                        '\$${cart.totalAmount}',
+                        style: TextStyle(
+                            color:
+                                Theme.of(context).primaryTextTheme.title.color),
+                      ),
+                      backgroundColor: Theme.of(context).primaryColor,
+                    ),
+                    Spacer(),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text('Order now'),
+                      textColor: Theme.of(context).primaryColor,
+                    )
+                  ],
+                )),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+              child: ListView.builder(
+            itemCount: cart.itemCount,
+            itemBuilder: (ctx, i) {
+              CartItem cartItem = cart.items.values.toList()[i];
+              String productId = cart.items.keys.toList()[i];
+              return CartListItem(
+                  productId: productId,
+                  id: cartItem.id,
+                  price: cartItem.price,
+                  quantity: cartItem.quantity,
+                  title: cartItem.title);
+            },
+          ))
         ],
       ),
     );
   }
-
 }
